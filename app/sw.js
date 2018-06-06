7ef23ddd6494a523fa78e1831d74fd21c7fbaa0b
sylvainpolletvillard <sylvain.pollet.villard@gmail.com>
2018-06-06 15:15:52 +0200

    etape 3

--- app/sw.js
+++ app/sw.js
@@ -1,7 +1,31 @@
+const CACHE_NAME = 'V1';
+const STATIC_CACHE_URLS = ['/', 'styles.css', 'scripts.js'];
+
 self.addEventListener('install', event => {
 	console.log('Service Worker installing.');
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(cache => cache.addAll(STATIC_CACHE_URLS))
+	)
 });
 
 self.addEventListener('activate', event => {
 	console.log('Service Worker activating.');
+	// delete any unexpected caches
+	event.waitUntil(
+		caches.keys()
+		.then(keys => keys.filter(key => key !== CACHE_NAME))
+		.then(keys => Promise.all(keys.map(key => {
+			console.log(`Deleting cache ${key}`);
+			return caches.delete(key)
+		})))
+	);
 });
+
+self.addEventListener('fetch', event => {
+	// Stratégie Cache-First
+	event.respondWith(
+		caches.match(event.request) // On vérifie si la requête a déjà été mise en cache
+		.then(cached => cached || fetch(event.request)) // sinon on requête le réseau
+	);
+})
